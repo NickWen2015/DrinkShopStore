63382d73b4405b25de2309253943185b991ad364
Nick Wen <y2ka.tw@gmail.com>
2018-08-19 10:52:10 +0800

    飲料商城商家端app放到雲端

---
+++ app/src/main/java/drinkshop/cp102/drinkshopstore/MainActivity.java
@@ -0,0 +1,13 @@
+package drinkshop.cp102.drinkshopstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
